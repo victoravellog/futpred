0c49137bfa0127f83529d47db8553b7cc7821fc1
Victor Avello <victoravellog@gmail.com>
2026-05-12 18:39:13 +0100

    Fix rubocop array bracket spacing

--- db/migrate/20260512171456_change_unique_prediction_index.rb
+++ db/migrate/20260512171456_change_unique_prediction_index.rb
@@ -1,7 +1,7 @@
 class ChangeUniquePredictionIndex < ActiveRecord::Migration[8.0]
   def change
-    remove_index :predictions, [:user_id, :fixture_id]
-    add_index :predictions, [:user_id, :fixture_id, :organization_tournament_id],
+    remove_index :predictions, [ :user_id, :fixture_id ]
+    add_index :predictions, [ :user_id, :fixture_id, :organization_tournament_id ],
               unique: true, name: "idx_predictions_user_fixture_org_tournament"
   end
 end
